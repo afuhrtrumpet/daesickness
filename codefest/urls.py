f10a384e7d2201f82385ec2c2ca9edd57833ddb1
Kacper Wardega <kacper@wardega.com>
2014-04-11 18:54:08 -0400

    templates

--- codefest/urls.py
+++ codefest/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,4 +17,10 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     
     url(r'^$', 'codefest.views.home',name='home')
+)
+
+
+urlpatterns += patterns('',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, }),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, }),
 )
