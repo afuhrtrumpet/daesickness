bd8ab91f41d94cb5b73e2eb49485930cac836839
Alex Fuhr <afuhrtrumpet>
2014-04-11 18:15:00 -0400

    Added alex's directory to db dirs

--- codefest/settings.py
+++ codefest/settings.py
@@ -13,6 +13,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '/home/kacper/workspace/codefest/sqlite.db',                      # Or path to database file if using sqlite3.
+	'/home/alex/codefest/sqlite.db',
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
