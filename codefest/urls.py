ad9607c648441f38105b0b22e4472be9f5bcae08
Alex Fuhr <afuhrtrumpet>
2014-04-11 18:56:25 -0400

    Added search functionality, not working yet

--- codefest/urls.py
+++ codefest/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^$', 'codefest.views.home',name='home')
+    url(r'^$', 'codefest.views.home',name='home'),
+    url(r'^search/$', 'codefest.views.search', name='search'),
 )
--- codefest/views.py
+++ codefest/views.py
@@ -1,7 +1,11 @@
 from django.template import loader, RequestContext
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 def home(request):
     t = loader.get_template('home.html')
     c = RequestContext(request, {})
     return HttpResponse(t.render(c))
+
+def search(request):
+	term = request.POST.get('term')
+	return HttpRedirectResponse(term)
