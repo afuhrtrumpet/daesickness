f33af866c94daf7f6b3bd0c21534821a8aca3a58
William Osler <william@oslers.us>
2014-04-12 02:17:40 -0400

    fix HTML entities being double escaped in HealthFinder results

--- codefest/parsers/healthfinder.py
+++ codefest/parsers/healthfinder.py
@@ -1,6 +1,7 @@
 import requests
 import codefest.keys as keys
 import xml.etree.ElementTree as ET
+import HTMLParser
 
 def parse(query):
 	url = "http://healthfinder.gov/developers/Search.xml"
@@ -10,7 +11,8 @@ def parse(query):
 	results = []
 	for topic in root.find('Topics').findall('Topic'):
 		result = {}
-		result['title'] = topic.find('Title').text
+                h = HTMLParser.HTMLParser()
+		result['title'] = h.unescape(topic.find('Title').text)
 		result['url'] = topic.find('AccessibleVersion').text
 		results.append(result)
 	healthfinder_item = {'title': 'Healthfinder', 'site_url': 'http://www.healthfinder.gov', 'icon_url': '/media/img/hf-apple.png', 'results': results}
