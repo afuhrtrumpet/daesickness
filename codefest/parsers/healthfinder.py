7525307a239d821c4538244239cc8ed11a0bba94
Alex Fuhr <afuhrtrumpet>
2014-04-11 22:03:34 -0400

    Added healthfinder parser

--- codefest/parsers/healthfinder.py
+++ codefest/parsers/healthfinder.py
@@ -1,17 +1,17 @@
 import requests
-import keys
+import codefest.keys as keys
 import xml.etree.ElementTree as ET
 
 def parse(query):
 	url = "http://healthfinder.gov/developers/Search.xml"
 	params = {'api_key':keys.HEALTHFINDER, 'keyword':query}
-	api_response = requests.get(url, params)
-	root = ET.fromstring(api_response)
+	api_response = requests.get(url, params=params)
+	root = ET.fromstring(api_response.text)
 	results = []
-	for topic in root[0].findall('topic'):
+	for topic in root.find('Topics').findall('Topic'):
 		result = {}
-		result['title'] = topic.find('Title')
-		result['url'] = topic.find('AccessibleVersion')
+		result['title'] = topic.find('Title').text
+		result['url'] = topic.find('AccessibleVersion').text
 		results.append(result)
-	healthfinder_item = {'site_url': 'reddit.com', 'icon_url': '', sources: results}
+	healthfinder_item = {'title': 'Healthfinder', 'site_url': 'healthfinder.gov', 'icon_url': '', 'results': results}
 	return healthfinder_item
--- codefest/views.py
+++ codefest/views.py
@@ -1,6 +1,7 @@
 from django.template import loader, RequestContext
 from django.http import HttpResponse
 import parsers.reddit as Reddit
+import parsers.healthfinder as HF
 
 def home(request):
     t = loader.get_template('home.html')
@@ -11,6 +12,7 @@ def search(request):
     t = loader.get_template('results.html')
     term = request.POST.get('term')
     reddit = Reddit.parse(term)
-    sources = [reddit,reddit]
+    healthfinder = HF.parse(term)
+    sources = [reddit,healthfinder]
     c = RequestContext(request, {'sources': sources})
     return HttpResponse(t.render(c))
