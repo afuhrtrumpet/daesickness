7de66d15faf4634c385ad26595213d56a1410869
Kacper Wardega <kacper@wardega.com>
2014-04-12 01:56:36 -0400

    added db

---
+++ bulletin_board/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class BulletinBoard(models.Model):
+	condition = models.CharField(max_length=50)
+
+	def __unicode__(self):
+		return '{0}'.format(self.condition)
+
+class Message(models.Model):
+	container = models.ForeignKey(BulletinBoard)
+	message = models.CharField(max_length=200)
+	pub_date = models.DateField(auto_now=True)
+
+	def __unicode__(self):
+		return self.message
